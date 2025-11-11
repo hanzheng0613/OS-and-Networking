58e0aa8ed394e9331d286d250e3e72a683fef39a
Hanzheng Wang <105999716+hanzheng0613@users.noreply.github.com>
2025-11-10 20:35:43 -0800

    Add files via upload

--- ros2_ws/src/ros2_shm_demo/trace.py
+++ ros2_ws/src/ros2_shm_demo/trace.py
@@ -6,12 +6,12 @@ from datetime import datetime, timedelta
 bpf_text = """
 int trace_write(struct pt_regs *ctx) {
     u64 ts = bpf_ktime_get_ns() / 1000000;
-    bpf_trace_printk("%llu ms: FastDDS::write() called\\n", ts);
+    bpf_trace_printk("%llu ms: SharedMemTransport::send() called\\n", ts);
     return 0;
 }
 int trace_read(struct pt_regs *ctx) {
     u64 ts = bpf_ktime_get_ns() / 1000000;
-    bpf_trace_printk("%llu ms: FastDDS::take_next_sample() called\\n", ts);
+    bpf_trace_printk("%llu ms: SharedMemTransport::push_discard() called\\n", ts);
     return 0;
 }
 """
@@ -20,9 +20,9 @@ b = BPF(text=bpf_text)
 
 
 
-# Attach uprobes to your functions
-b.attach_uprobe(name="/opt/ros/humble/lib/libfastrtps.so.2.6.10",sym="_ZN8eprosima7fastdds3dds14DataWriterImpl5writeEPv", fn_name="trace_write")
-b.attach_uprobe(name="/opt/ros/humble/lib/libfastrtps.so.2.6.10",sym="_ZN8eprosima7fastdds3dds14DataReaderImpl12read_or_takeERNS1_18LoanableCollectionERNS1_16LoanableSequenceINS1_10SampleInfoESt17integral_constantIbLb1EEEEiRKNS_8fastrtps4rtps16InstanceHandle_tEtttbbb", fn_name="trace_read")
+b.attach_uprobe(name="/opt/ros/humble/lib/libfastrtps.so",sym="_ZN8eprosima7fastdds4rtps18SharedMemTransport4sendEPKhjPNS1_16LocatorsIteratorES6_RKNSt6chrono10time_pointINS7_3_V212steady_clockENS7_8durationIlSt5ratioILl1ELl1000000000EEEEEE", fn_name="trace_write")
+b.attach_uprobe(name="/opt/ros/humble/lib/libfastrtps.so",sym="_ZN8eprosima7fastdds4rtps18SharedMemTransport12push_discardERKSt10shared_ptrINS1_16SharedMemManager6BufferEERKNS_8fastrtps4rtps9Locator_tE", fn_name="trace_read")
+
 
 print("Tracing... Ctrl-C to exit.")
 boot_time = time.time() - (b.ksymtime / 1e6 if hasattr(b, "ksymtime") else 0)
